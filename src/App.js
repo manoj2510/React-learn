5adec7f40f1fb756485833db1c0d39a9f920154c
Manoj Chandrasekaran <chandrasekaran.ma@northeastern.edu>
2023-07-24 17:59:22 -0400

    pass data through props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 import './App.css';
-import GoalList from './components/GoalList';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
 
 const App = () => {
+  const courseGoals = [
+    {id: 'cg1', text: 'Finish the Course'},
+    {id: 'cg2', text: 'Learn all about the Course Main Topic'},
+    {id: 'cg3', text: 'Help other students in the Course Q&A'},
+  ];
+
   return (
   <div className="course-goals">
     <h2>Course Goals</h2>
-    <GoalList />
+    <NewGoal />
+    <GoalList goals={courseGoals}/>
   </div>
   );
 };
